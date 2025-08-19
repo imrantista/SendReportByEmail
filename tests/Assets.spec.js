5b7050f0a891aadf58f48b1f37f8c940f827d7ef
Golam Mostafa Imran <39483919+imrantista@users.noreply.github.com>
2025-08-19 12:41:27 +0600

    Update Assets.spec.js

--- tests/Assets.spec.js
+++ tests/Assets.spec.js
@@ -1,29 +1,28 @@
 import { test, expect } from "@playwright/test";
-import CommonActions from "../Pages/Common/Actions.js";
+import LoginSession from "../Pages/Common/LoginSession.js";
+import SendResult from "../Pages/Common/SendResult.js";
 import AssetDelete from "../Pages/Assets/AssetDelete.js";
 import testData from "../fixtures/testData.json" assert { type: "json" };
 test.describe("Assets Module Tests", () => {
   let resultTracker;
-  let commonAction;
-  test.beforeEach(async () => {
+  const loginSession = new LoginSession();
+  const deleteAsset = new AssetDelete();
+  const mailer = new SendResult();
+  test.beforeEach(async ({ page }) => {
     resultTracker = {
       successCount: 0,
       errorCount: 0,
       errorMessages: [],
       stepToReproduces: [],
     };
-    commonAction = new CommonActions();
-  });
-  test.beforeEach(async ({ page }) => {
-    await commonAction.loginSession(page);
-  });
-  test("Delete Asset", async ({ page }) => {
+    await loginSession.login(page);
     await page.goto(`${testData.link}products-and-assets?tab=Assets`);
     await expect(page.locator("text=Search")).toBeVisible();
-    const deleteAsset = new AssetDelete();
+  });
+  test("Delete Asset", async ({ page }) => {
     await deleteAsset.deleteAsset(page, resultTracker);
   });
   test.afterAll(async () => {
-    await commonAction.logResults(resultTracker, "Assets Module");
+    await mailer.logResults(resultTracker, "Assets Module");
   });
 });
